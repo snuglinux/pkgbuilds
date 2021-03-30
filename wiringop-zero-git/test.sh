349e7c45a1ef469a34f658a18bdabf5453d3791b
khvalera <khvalera@ukr.net>
2021-03-30 18:22:59 +0300

    wiringop-zero-git

--- wiringop-zero-git/test.sh
+++ wiringop-zero-git/test.sh
@@ -3,22 +3,34 @@
 # test.sh:
 # Simple test: Assumes LEDs on Pins 0-7 and lights them in-turn.
 
-for i in `seq 0 7`;
+mode=$1
+
+for i in `seq 0 10`;
 do
   gpio mode $i out
 done
 
 while true;
 do
-  for i in `seq 0 7`;
+  for i in `seq 0 10`;
   do
+    echo "gpio write $i 1"
     gpio write $i 1
-    sleep 0.1
+    if [ -z "$mode" ]; then
+       sleep 0.9;
+    elif [[ "$mode"=="read" ]]; then
+       read;
+    fi
   done
 
-  for i in `seq 0 7`;
+  for i in `seq 0 10`;
   do
+    echo "gpio write $i 0"
     gpio write $i 0
-    sleep 0.1
+    if [ -z "$mode" ]; then
+       sleep 0.9;
+    elif [[ "$mode"=="read" ]]; then
+       read;
+    fi
   done
 done
